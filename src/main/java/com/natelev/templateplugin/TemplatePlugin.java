fe6a881fed3e2b5fa889e4bc7b0bef77b4d2093b
Nathan Levy <nathan.levy.au@gmail.com>
2022-04-09 22:52:01 +1200

    added template plugin

---
+++ src/main/java/com/natelev/templateplugin/TemplatePlugin.java
@@ -0,0 +1,14 @@
+package com.natelev.templateplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TemplatePlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+    @Override
+    public void onDisable() {
+
+    }
+}
